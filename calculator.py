5fafdc4bbfb6feab89dce9ad2aa00002dbc3eab6
Jdebbie254 <djelagat148@gmail.com>
2025-04-08 22:46:37 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,21 @@
+num 1=float(input("Enter the first number:"))
+num 2=float(input("Enter the second number:"))
+operation=input("Enter the operation(+,-,*,/):")
+
+if operation=="+"
+result=num 1+num 2
+print(f"{num 1}+{num 2}={result}")
+elif operation=="-"
+result=num 1-num 2
+print(f"{num 1}-{num 2}={result}")
+elif operation=="*"
+result=num 1*num 2
+print(f"{num 1}-{num 2}={result}")
+elif operation=="/"
+if num 2 !=0;
+result = num 1/num 2
+print(f"{num 1}/{num 2}={result}")
+else;
+print("Error: Division by zero is not allowed.")
+else;
+print("Invalid operation")
